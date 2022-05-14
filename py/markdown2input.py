f4887271cfd92b36091c11e1419fb415c796edcd
Dodex <dodexpwnz@gmail.com>
2022-05-14 12:38:49 +0300

    add utf8

--- py/markdown2input.py
+++ py/markdown2input.py
@@ -3,7 +3,7 @@ import csv
 
 def inputmd(fieldname=None):
     os.chdir(os.path.dirname(__file__))
-    with open("../csv/wb_dquotes.csv", newline="") as fi:
+    with open("../csv/wb_dquotes.csv", newline="", encoding="utf-8") as fi:
         mydict = csv.DictReader(fi, delimiter=";")
         for row in mydict:
             if fieldname == None:
